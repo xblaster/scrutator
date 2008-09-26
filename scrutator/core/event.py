1f26b08cb9351f89dee8c13cf0c044278848d24e
Jerome Wax <jerome.wax@lo2k.net>
2008-09-26 17:23:23 +0200

    event serializer

--- scrutator/core/event.py
+++ scrutator/core/event.py
@@ -1,15 +1,49 @@
-class SimpleEvent:
-    """simple event
-    base class of all event"""
-    def __init__(self, arg = []):
-        self.arg = arg
+import scrutator.core.exception
+from scrutator.core.tool import smart_load
 
-    def getType(self):
-        return self.__class__.__name__
-        
+class EventSerializer(object):
+	def __init__(self):
+		pass
+	
+	def event2array(self, obj):
+		if not isinstance(obj, SimpleEvent):
+			raise scrutator.core.exception.BadTypeException("Not a SimpleEvent")
+		return dict(type=obj.getType(), arg = obj.getArg())
+		
+	def array2event(self, refDict):
+		if not refDict.has_key('type'):
+			raise scrutator.core.exception.BadTypeException("Do not contain a type for reconstruction")
+		recons = smart_load(refDict['type'])()
+		recons.setArg(refDict['arg'])
+		
+		return recons
+
+class SimpleEvent:
+	"""simple event
+	base class of all event"""
+	def __init__(self, arg = dict()):
+		self.setArg(arg)
+
+	def getType(self):
+		"""return the type of the object (containing module name and class name)"""
+		return self.__class__.__module__+'.'+self.__class__.__name__
+		
+	def getArg(self):
+		return self.arg
+		
+	def setArg(self,argDict):
+		self.arg = argDict
+	
+	def getArgEntry(self, entry):
+		return self.arg[entry]
+	
+	def setArgEntry(self, entryname, entry):
+		self.arg[entryname] = entry
+	
+		
 class KickEvent(SimpleEvent):
-    pass
-    
+	pass
+	
 class BanEvent(SimpleEvent):
-    pass
+	pass
 
--- scrutator/tests/events.py
+++ scrutator/tests/events.py
@@ -2,14 +2,43 @@ import unittest
 from scrutator.core.event import *
 
 class TestEvents(unittest.TestCase):
-    def setUp(self):
-        pass
-        
-    def testSimpleEvent(self):
-        s = SimpleEvent()
-        self.assertEqual( s.getType(), "SimpleEvent")
-        
-    def testInheritedEvent(self):
-        s = KickEvent()
-        self.assertEqual( s.getType(), "KickEvent")
-        
+	def setUp(self):
+		pass
+		
+	def getMockupEvent(self):
+		s = BanEvent(dict(channel="#funradio", nickname="jdl"))
+		return s
+	
+	def testSimpleEvent(self):
+		s = SimpleEvent()
+		self.assertEqual( s.getType(), "scrutator.core.event.SimpleEvent")
+		
+	def testInheritedEvent(self):
+		s = KickEvent()
+		self.assertEqual( s.getType(), "scrutator.core.event.KickEvent")
+
+	
+	def testEventArgs(self):
+		s = self.getMockupEvent()
+		self.assertEqual(s.getArgEntry('channel'),"#funradio")
+		
+		s.setArgEntry('count',4)
+		self.assertEqual(s.getArgEntry('count'),4)
+		
+	def testEventSerializerEvent2Arr(self):
+		s = self.getMockupEvent()
+		es = EventSerializer()
+		res = es.event2array(s)
+		self.assertEqual(res['type'], s.getType())
+		self.assertEqual(res['arg'], s.getArg())
+	
+	def testEventSerializerArr2Event(self):
+		s = self.getMockupEvent()
+		es = EventSerializer()
+		res = es.event2array(s)
+		
+		s_reconstruct = es.array2event(res)
+			
+		if not isinstance(s_reconstruct, BanEvent):
+			self.fail('Bad reconstruction :)')
+		
