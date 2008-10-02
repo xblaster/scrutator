62dbc092ba7a5ab0cceaf926d96dac12b46d9116
Jerome Wax <jerome.wax@lo2k.net>
2008-10-02 18:56:28 +0200

    add xmlrpc client rename server to network

--- scrutator/core/network.py
+++ scrutator/core/network.py
@@ -14,7 +14,7 @@ class SCRTServices(xmlrpc.XMLRPC):
 		"""Push event"""
 		es = EventSerializer()
 		for obj in obj_list:
-			res = es.event2array(obj_list)
+			res = es.array2event(obj_list)
 			s_reconstruct = es.array2event(res)
 			manager.push(s_reconstruct)
 
@@ -22,7 +22,20 @@ class SCRTServices(xmlrpc.XMLRPC):
 		"""Pull event to a destination"""
 		return null
 
-class SCRTXMLRPC:
+class XMLRPCClient:
+	def __init__(self, serviceuri):
+		import xmlrpclib
+		self.xmlrpc_connect = xmlrpclib.ServerProxy(serviceuri)
+		
+	def push(self, eventObj):
+		es = EventSerializer()
+		res = es.event2array(eventObj)
+		send_list = list()
+		send_list.append(res)
+		
+		self.xmlrpc_connect.push(send_list)
+
+class XMLRPCServer:
 	def __init__(self, service, port, eventMgrObj):
 		from twisted.internet import reactor
 		r = service
