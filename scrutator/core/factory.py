922f3df13e51e3c6477b03893cefe8c3afc76e07
Jerome Wax <jerome.wax@lo2k.net>
2008-10-23 07:58:11 +0200

    add comment

--- scrutator/core/factory.py
+++ scrutator/core/factory.py
@@ -26,23 +26,37 @@ class XMLBeanFactory(AbstractBeanFactory):
 			self.loadBean(bean)
 	
 	def loadBean(self, bean):
+		"""load a bean"""
 		bean_id  = bean.getAttribute('id')
 		class_name = bean.getAttribute('class')
 		
 		argument_list = list()
 		
+		#fetch element in constructor arg
 		args = bean.getElementsByTagName('constructor-arg')
+		
+		#if we have params for constructor
 		if len(args) == 1:
 			arg = args[0]
 			for node in arg.childNodes:
+				
+				#fetch value
 				if (node.nodeName == "value"):
 					val = getText(node.childNodes)
+					
+					#if value is an int, convert it
 					if val.isdigit():
 						val = int(val)
+						
+					#append element to argument list
 					argument_list.append(val)
+				
+				#if we have reference element, fetch it with the getBean method
+				#warning: you must declare reference before using it
 				if (node.nodeName == "ref"):
 					argument_list.append(CoreManager().getBean(node.getAttribute("bean")))
-					
+		
+		#smart load reference of the class			
 		class_obj = smart_load(class_name)
 		try:
 			if (len(argument_list)==0):
@@ -52,6 +66,7 @@ class XMLBeanFactory(AbstractBeanFactory):
 		except TypeError, e:
 			raise XMLBeanConstArgError(class_obj.__name__+" bad args "+str(argument_list)+"\n"+str(e))
 		
+		#affect properties
 		for property in bean.getElementsByTagName('property'):
 			if len(property.getElementsByTagName('value')) > 0:
 				value = getText(property.getElementsByTagName('value')[0].childNodes)
