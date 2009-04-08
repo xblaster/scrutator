8c9be8c8a9d037f8173f4d580c2ea84ebb262e33
Jerome Wax <jerome.wax@lo2k.net>
2009-04-08 18:22:51 +0200

    fail failed ;)

--- scrutator/tests/tool.py
+++ scrutator/tests/tool.py
@@ -6,5 +6,5 @@ class TestTool(unittest.TestCase):
 		
 	def testSmartLoad(self):
 		if not isinstance(smart_load('scrutator.core.event.SimpleEvent')(), sys.modules['scrutator'].core.event.SimpleEvent):
-			fail('smartLoad error')
+			self.fail('smartLoad error')
 		
