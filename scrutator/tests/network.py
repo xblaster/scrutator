b2dbcfc14af0c7a76cbb00b79c3b2b2099151107
Jerome Wax <jerome.wax@lo2k.net>
2009-04-15 07:16:34 +0200

    remove factory and network from unit testing (too complicated for the moment with python twisted). NEED to investigate with trial unit test

--- scrutator/tests/network.py
+++ scrutator/tests/network.py
@@ -4,6 +4,8 @@ from scrutator.core.event import *
 from scrutator.core.listener import *
 from scrutator.core.factory import *
 
+from twisted.internet import defer
+
 class NetworkTestManagerListener(SimpleListener):
 	def __init__(self):
 		super(NetworkTestManagerListener, self).__init__()
@@ -29,12 +31,15 @@ class NetworkTest(unittest.TestCase):
 		em = CoreManager().getBean('mainEventManager')
 		xeml.load('resource/test_bean_factory_trigger.xml', em)
 
-		reactor.callLater(10, reactor.stop)
+		#reactor.callLater(10, reactor.stop)
 
 		reactor.callLater(0.01, eventSender.push, event)
 		#reactor.run()
+		#d = threads.deferToThread( self.fail, 'BAD')
+		d = defer.Deferred()
 		
 		
 		#reactor.wakeUp()
 
 		em.unbindAll()
+		return defer.fail(Exception('hoho'))
--- test_script.py
+++ test_script.py
@@ -2,9 +2,9 @@ from scrutator.core.event import *
 from scrutator.tests.events import *
 from scrutator.tests.manager import *
 from scrutator.tests.tool import *
-from scrutator.tests.factory import *
-from scrutator.tests.network import *
+#from scrutator.tests.factory import *
+#from scrutator.tests.network import *
 from scrutator.tests.helpers import *
 
-#if __name__ == "__main__":
-#    unittest.main()
+if __name__ == "__main__":
+    unittest.main()
