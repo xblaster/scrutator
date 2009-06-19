534a6e6f4ee544f3b1d907d95134df342a4587a2
Jérôme Wax <jeromewax@macbook-de-jerome-wax.local>
2009-06-19 22:21:18 +0200

    add ihook to auto load all by network

--- scrutator/core/sync/__init__.py
+++ scrutator/core/sync/__init__.py
@@ -2,3 +2,27 @@
 #remove upload dir
 import shutil
 shutil.rmtree('upload', True)
+from scrutator.core.tool import *
+
+
+#install hook
+import ihooks
+
+class SyncHook(ihooks.ModuleImporter):
+    def import_module(self, name, globals=None, locals=None, fromlist=None, level = -1):
+		if fromlist:
+			print 'syncHOOK '+name+' '+str(fromlist)
+		try:
+			imp = ihooks.ModuleImporter.import_module(self, name, globals, locals, fromlist)
+		except:
+			smart_import(name)
+			imp = ihooks.ModuleImporter.import_module(self,name, globals, locals, fromlist)
+		return imp
+		
+
+
+
+print 'Install sync_hook'
+SyncHook().install()
+
+import sys
