582cf53cad3444afac723133111c36f00f545989
xblaster <xblaster@lo2k.net>
2009-11-19 13:08:42 +0100

    seems to work fine now (strange)

--- scrutator/core/sync/__init__.py
+++ scrutator/core/sync/__init__.py
@@ -8,19 +8,32 @@ from scrutator.minidi.tool import *
 #install hook
 import ihooks
 
+#class SyncHook(ihooks.ModuleImporter):
+#    def import_module(self, name, globals=None, locals=None, fromlist=None, level= -1):
+#        try:
+#            imp = ihooks.ModuleImporter.import_module(self, name, globals, locals, fromlist)
+#        #except (NameError, ImportError) as e:
+#        except:
+#            try:
+#                smart_import(name)
+#                imp = ihooks.ModuleImporter.import_module(self, name, globals, locals, fromlist)
+#            except (Exception), e:
+#                log.msg("Error during smart_import" +str(e))
+#                pass
+#        return imp
 class SyncHook(ihooks.ModuleImporter):
-    def import_module(self, name, globals=None, locals=None, fromlist=None, level= -1):
+    def import_module(self, name, globals=None, locals=None, fromlist=None, level = -1):
+        if fromlist:
+            print 'syncHOOK '+name+' '+str(fromlist)
         try:
             imp = ihooks.ModuleImporter.import_module(self, name, globals, locals, fromlist)
-        #except (NameError, ImportError) as e:
-        except:
-            try:
-                smart_import(name)
-                imp = ihooks.ModuleImporter.import_module(self, name, globals, locals, fromlist)
-            except (Exception), e:
-                log.msg("Error during smart_import" +str(e))
-                pass
+        except (Exception), e:
+            log.msg("Catched Exception")
+            import traceback
+            traceback.print_exc(file=sys.stdout)
+
+            smart_import(name)
+            imp = ihooks.ModuleImporter.import_module(self,name, globals, locals, fromlist)
         return imp
-    
-print 'Install sync_hook'
-SyncHook().install()
+
+
--- scrutator/tests/tool.py
+++ scrutator/tests/tool.py
@@ -1,6 +1,14 @@
+from scrutator.core.manager import EventManager
+from scrutator.core.sync.listener import FileRequestListener
+#from scrutator.core.sync.event import FileContent
 import unittest
 from scrutator.minidi.tool import *
 import sys
+import xml.dom.minidom
+
+
+
+#import scrutator.core.sync
 
 class TestTool(unittest.TestCase):
 		
@@ -8,3 +16,11 @@ class TestTool(unittest.TestCase):
 		if not isinstance(smart_load('scrutator.core.event.SimpleEvent')(), sys.modules['scrutator'].core.event.SimpleEvent):
 			self.fail('smartLoad error')
 		
+	def testInjection(self):
+		f = open('scrutator/tests/tool.fakepy', 'r')
+		
+		content = f.read()
+		
+		#bus = EventManager()
+		#FileRequestListener()
+		#bus.bind(FileContent().getType(),FileRequestListener())
